73deddae2b606872a3ee7a8ad37c21d792f1d482
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-25 20:29:30 -0300

    Firebase file renamed and moved to /middleware

--- src/middleware/Firebase.ts
+++ src/middleware/Firebase.ts
@@ -1,10 +1,8 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAnalytics } from 'firebase/analytics';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,7 +16,6 @@ const {
   REACT_APP_FIREBASE_APP_ID,
   REACT_APP_FIREBASE_APP_MEASUREMENT_ID,
 } = process.env;
-
 const firebaseConfig = {
   apiKey: `${REACT_APP_FIREBASE_APP_API_KEY}`,
   authDomain: `${REACT_APP_FIREBASE_APP_AUTH_DOMAIN}`,
@@ -28,7 +25,15 @@ const firebaseConfig = {
   appId: `${REACT_APP_FIREBASE_APP_ID}`,
   measurementId: `${REACT_APP_FIREBASE_APP_MEASUREMENT_ID}`,
 };
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Firestore database
 export const db = getFirestore(app);
+
+// Analyctics
 export const analytics = getAnalytics(app);
