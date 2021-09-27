2d3c8cbf50ccb95c4d9d69be71311983000859d7
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-27 18:36:38 -0300

    Task list without file

--- src/Components/Task/AllTasks.tsx
+++ src/Components/Task/AllTasks.tsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Typography, Button, Box } from '@mui/material';
+import { changeTitle } from '../../helpers/modifyHtmlHead';
 import Style from './Tasks.module.scss';
 import Task from './Task';
+import List from './List';
 
 export default function AllTasks() {
+  changeTitle('Tarefas');
   return (
     <>
       <Task>
-        <Box className={Style.task__title}>
-          <Typography variant="h1" className={Style.task__title__text}>
-            Tarefas
-          </Typography>
-          <Link to="/tarefa/nova" className={Style.task__link}>
-            <Button variant="contained" className={Style.task__title__button}>
-              Nova tarefa
-            </Button>
-          </Link>
-        </Box>
+        <>
+          <Box className={Style.task__title}>
+            <Typography variant="h1" className={Style.task__title__text}>
+              Tarefas
+            </Typography>
+            <Link to="/tarefa/nova" className={Style.task__link}>
+              <Button variant="contained" className={Style.task__title__button}>
+                Nova tarefa
+              </Button>
+            </Link>
+          </Box>
+          <List />
+        </>
       </Task>
     </>
   );
---
+++ src/Components/Task/List.tsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Box, List, ListItem, Typography, IconButton } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
+
+import { getAllDocuments } from '../../middleware/Firebase/Firestore';
+import Style from './Tasks.module.scss';
+
+interface ITask {
+  title: string;
+  description: string;
+  status: string;
+  responsible: string;
+  id: string;
+}
+
+export default function ListTasks() {
+  const [allTasks, setAllTasks] = useState<ITask[]>([]);
+  const getAllTasks = async () => {
+    const tasks = await getAllDocuments('tasks');
+    tasks.sort((a, b) => a.title.localeCompare(b.title));
+    setAllTasks(tasks);
+  };
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+  if (!allTasks.length) {
+    return <></>;
+  }
+  console.log(allTasks);
+  return (
+    <List>
+      {allTasks.map((task) => (
+        <div className={Style.list} key={task.id}>
+          <ListItem disablePadding className={Style.list__item}>
+            <div className={Style.list__item__container}>
+              <Typography variant="h5">
+                {task.title} ({task.status})
+              </Typography>
+            </div>
+            <div className={Style.list__item__container}>
+              <Typography variant="h6">{task.description}</Typography>
+            </div>
+          </ListItem>
+          <span className={Style.list__file}>Arquivo</span>
+          <IconButton aria-label="edit" size="large">
+            <Link
+              to={`/tarefa/${task.id}`}
+              style={{ color: 'rgba(0, 0, 0, 0.54)' }}
+            >
+              <Edit fontSize="inherit" />
+            </Link>
+          </IconButton>
+          <IconButton aria-label="delete" size="large">
+            <Delete fontSize="inherit" />
+          </IconButton>
+        </div>
+      ))}
+    </List>
+  );
+}
