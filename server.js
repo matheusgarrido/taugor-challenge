57a33e1745fe61331a3cb34a26c5fc8efb35ace4
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-28 04:10:15 -0300

    Adjust server port value

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/', express.static(resolve(__dirname, './build')));
 
-app.listen(process.env.POR || 300, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
     return console.log(err);
   }
