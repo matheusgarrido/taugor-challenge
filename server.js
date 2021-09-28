9984d1bade0888c1d89deac5ecf5de25035c15ba
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-28 03:55:02 -0300

    Express server config

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+
+app.use('/', express.static(resolve(__dirname, './build')));
+
+app.listen(process.env.POR || 300, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  return console.log('Express running');
+});
