4fe8115cb91d91bd175e293a232e7d7661450e70
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-09-29 16:27:14 -0300

    Adjusted express error getting route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { resolve } = require('path');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(resolve(__dirname, './build')));
+const publicPath = path.join(__dirname, './build');
+app.use(express.static(publicPath));
+app.use('*', express.static(publicPath));
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
